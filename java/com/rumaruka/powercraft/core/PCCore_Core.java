a1e0a00458e04b1ddd220af9d30df9eda8d70847
Rumaruka <rumarukakunnya@gmail.com>
2019-02-27 00:15:40 +0500

    Revert "Add First Item"
    This reverts commit 65380c6884301169c1c6bbe1c6f4c374b78f0caa.

--- java/com/rumaruka/powercraft/core/PCCore_Core.java
+++ java/com/rumaruka/powercraft/core/PCCore_Core.java
@@ -1,12 +1,42 @@
 package com.rumaruka.powercraft.core;
 
-public class PCCore_Core   {
+import com.rumaruka.powercraft.api.PCApi;
+import com.rumaruka.powercraft.api.PCBuild;
+import com.rumaruka.powercraft.api.PCModule;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
 
+
+@Mod(modid = PCCore_Core.NAME,name = PCCore_Core.NAME,version = PCCore_Core.VERSION,dependencies = PCCore_Core.DEPENDENCIES)
+public class PCCore_Core extends PCModule {
 
     public static final String NAME = POWERCRAFT + "-Core";
     public static final String VERSION = PCBuild.BUILD_VERSION;
     public static final String DEPENDENCIES = "required-after:" + PCApi.NAME + "@" + PCApi.VERSION;
 
-    public static final PCCore_Core INSTANCE = new PCCore_Core();
+    public static PCCore_Core instance;
 
+    static   {
+        try {
+            instance = new PCCore_Core();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Mod.InstanceFactory
+    public static PCCore_Core factory(){
+        return instance;
+    }
+
+    public PCCore_Core() throws IllegalAccessException {
+
+    }
+
+
+
+    @Override
+    public ItemStack getCreativeTabItemStack() {
+        return null;
+    }
 }
